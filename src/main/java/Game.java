238c075aa39baaa3adb79d4f9ff5c981a22432c8
chlduq04 <chlduq04@gmail.com>
2024-04-29 12:52:57 +0900

    [feature] 63

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,7 +7,15 @@ public class Game {
         if (guessNumber.equals(question)) {
             return new GuessResult(true, 3, 0);
         } else {
-            return new GuessResult(false, 0,0);
+            int strikes = 0;
+            for (int i = 0; i < question.length(); i++) {
+                char charAt = guessNumber.charAt(i);
+                int index0f = question.indexOf(charAt);
+                if (index0f == i) {
+                    strikes++;
+                }
+            }
+            return new GuessResult(false, strikes,0);
         }
     }
 
