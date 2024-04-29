4b4a0751f645276d432f87422ad1a75b28fbcdb4
chlduq04 <chlduq04@gmail.com>
2024-04-29 10:43:41 +0900

    [refactoring] 29

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,19 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void createGame() {
-        Game game = new Game();
         assertNotNull(game);
     }
 
     @Test
     void throwExceptionWhenInputIsNull() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
-            game.quess(null);
+            game.guess(null);
         });
     }
+
 }
